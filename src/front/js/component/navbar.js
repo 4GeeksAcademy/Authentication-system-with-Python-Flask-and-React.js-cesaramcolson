7737c75b2bfd5545efa004efacd33cfbd4a79411
Cesar Amaya Colella <cesar.rafael.gep@gmail.com>
2024-08-14 02:02:35 +0000

    some styling

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -12,29 +12,33 @@ export const Navbar = () => {
 
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <Link className="navbar-brand" to="/">Home</Link>
-            <div className="collapse navbar-collapse">
-                <ul className="navbar-nav mr-auto">
-                    {store.token ? (
-                        <>
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/private">Profile</Link>
-                            </li>
-                            <li className="nav-item">
-                                <button className="btn btn-secondary" onClick={handleLogout}>Logout</button>
-                            </li>
-                        </>
-                    ) : (
-                        <>
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/login">Login</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/signup">Signup</Link>
-                            </li>
-                        </>
-                    )}
-                </ul>
+            <div className="container">
+                <div className="d-flex justify-content-between w-100">
+                    <Link className="navbar-brand" to="/">Home</Link>
+                    <div className="d-flex">
+                        <ul className="navbar-nav">
+                            {store.token ? (
+                                <>
+                                    <li className="nav-item">
+                                        <Link className="nav-link" to="/private">Profile</Link>
+                                    </li>
+                                    <li className="nav-item">
+                                        <button className="btn btn-danger" onClick={handleLogout}>Logout</button>
+                                    </li>
+                                </>
+                            ) : (
+                                <>
+                                    <li className="nav-item">
+                                        <Link className="nav-link" to="/login">Login</Link>
+                                    </li>
+                                    <li className="nav-item">
+                                        <Link className="nav-link" to="/signup">Signup</Link>
+                                    </li>
+                                </>
+                            )}
+                        </ul>
+                    </div>
+                </div>
             </div>
         </nav>
     );
